a980eef176b38ef1a5da7fbe991fba937b00b9d4
jgnacio <jgnaciogomez@gmail.com>
2024-05-03 16:52:08 -0300

    scene component, add light helpers, and update board update. "Life"

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -8,23 +8,35 @@ import {
   useHelper,
 } from "@react-three/drei";
 import { useEffect, useRef, useState, type ReactElement } from "react";
-import { DirectionalLightHelper, Mesh } from "three";
+import { DirectionalLightHelper, AxesHelper, Mesh } from "three";
+
 import Scene01 from "./scene01";
 
 type board = number[][];
 
 function createBoard(rows: number, cols: number): board {
   return Array.from({ length: rows }, () =>
-    Array.from({ length: cols }, () => 1)
+    Array.from({ length: cols }, () => (Math.random() - 0.3 > 0.5 ? 1 : 0))
+  );
+}
+
+function clearBoard(rows: number, cols: number): board {
+  return Array.from({ length: rows }, () =>
+    Array.from({ length: cols }, () => 0)
   );
 }
 
 const Scene02 = () => {
   const LightHelperRefScene02 = useRef();
-  useHelper(LightHelperRefScene02, DirectionalLightHelper, 0.5, "white");
+  const LightHelperRefScene03 = useRef();
+  const LightHelperRefScene04 = useRef();
 
-  const rows = 8;
-  const cols = 8;
+  useHelper(LightHelperRefScene02, DirectionalLightHelper, 0.5, "yellow");
+  useHelper(LightHelperRefScene03, DirectionalLightHelper, 0.5, "red");
+  useHelper(LightHelperRefScene04, DirectionalLightHelper, 0.5, "blue");
+
+  const rows = 20;
+  const cols = 20;
   const cellSize = 0.2;
   const fill = [
     <></>,
@@ -36,35 +48,81 @@ const Scene02 = () => {
   const board = createBoard(rows, cols);
   const [boardState, setBoardState] = useState<board>(board);
 
-  useFrame((state, delta) => {
+  useEffect(() => {
     if (boardState) {
-      // rellenar el tablero de etiquetas vacias
-      for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-          // console.log(Math.abs(Math.round(Math.sin(state.clock.elapsedTime))));
-          const newBoardState = [...boardState];
-          newBoardState[i][j] = Math.abs(
-            Math.round(Math.sin(state.clock.elapsedTime - j))
-          );
-          setBoardState(newBoardState);
+      setTimeout(() => {
+        computeNextBoard();
+      }, 50);
+    }
+  }, [boardState]);
 
-          // console.table(boardState);
+  function countNeighbors(x: number, y: number) {
+    let sum = 0;
+    for (let i = -1; i <= 1; ++i) {
+      for (let j = -1; j <= 1; ++j) {
+        if (i === 0 && j === 0) {
+          continue;
+        }
+        const row = (x + i + rows) % rows;
+        const col = (y + j + cols) % cols;
+        if (boardState[row][col] === 1) {
+          sum++;
         }
       }
     }
-  });
+    return sum;
+  }
+
+  function computeNextBoard() {
+    const nextBoard = clearBoard(rows, cols);
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        const neighbors = countNeighbors(i, j);
+        if (boardState[i][j] === 1) {
+          if (neighbors < 2 || neighbors > 3) {
+            nextBoard[i][j] = 0;
+          } else {
+            nextBoard[i][j] = 1;
+          }
+        } else {
+          if (neighbors === 3) {
+            nextBoard[i][j] = 1;
+          }
+        }
+      }
+    }
+    setBoardState(nextBoard);
+  }
 
   return (
     <>
-      <directionalLight position={[0, 1, 4]} ref={LightHelperRefScene02} />
-      <ambientLight intensity={0.5} />
+      <directionalLight
+        position={[0, 1, 4]}
+        ref={LightHelperRefScene02}
+        color={"yellow"}
+      />
+      <directionalLight
+        position={[5, 4, 0]}
+        ref={LightHelperRefScene03}
+        color={"red"}
+      />
+      <directionalLight
+        position={[-5, 4, 0]}
+        ref={LightHelperRefScene04}
+        color={"blue"}
+      />
+      <ambientLight intensity={0.5} color={"#444444"} />
       {boardState.map((row, i) =>
         row.map((cell, j) => (
-          <mesh position={[i * cellSize, j * cellSize, 0]} key={i + 1 * j + 1}>
+          <mesh
+            onClick={computeNextBoard}
+            position={[i * cellSize, j * cellSize, 0]}
+            key={`${i}-${j}`}
+          >
             {cell === 1 ? (
               <>
                 <boxGeometry args={[cellSize, cellSize, cellSize]} />
-                <meshBasicMaterial />
+                <meshStandardMaterial color={"white"} />
               </>
             ) : (
               ""
